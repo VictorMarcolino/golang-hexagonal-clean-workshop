53c860056ad9cb369d92919d6389c027b80af5aa
Victor Marcolino <Victor.Marcolino@swisscom.com>
2023-10-19 18:27:21 +0200

    [STEP 6] Use the adapter on the tests

--- tests/unit/ports/create_candidate_test.go
+++ tests/unit/ports/create_candidate_test.go
@@ -1,12 +1,13 @@
 package ports_test
 
 import (
+	"github.com/VictorMarcolino/golang-hexagonal-clean-workshop/pkg/adapters"
 	"github.com/VictorMarcolino/golang-hexagonal-clean-workshop/tests/specs"
 
 	. "github.com/onsi/ginkgo/v2"
 )
 
-
 var _ = Describe("PORT:IN-MEMORY:CreateCandidate", func() {
-	specs.CreateCandidatePort(nil)
+	adapter := adapters.NewInMemoryAdapter()
+	specs.CreateCandidatePort(adapter)
 })
--- tests/unit/usecases/create_candidate_test.go
+++ tests/unit/usecases/create_candidate_test.go
@@ -1,10 +1,15 @@
 package usecases_test
 
 import (
+	"github.com/VictorMarcolino/golang-hexagonal-clean-workshop/pkg/adapters"
+	"github.com/VictorMarcolino/golang-hexagonal-clean-workshop/pkg/core/usecases"
 	"github.com/VictorMarcolino/golang-hexagonal-clean-workshop/tests/specs"
 	. "github.com/onsi/ginkgo/v2"
 )
 
-var _ = Describe("CreateCandidateUsecase", func() {
-	specs.CreateCandidateUsecase(nil)
+var _ = Describe("USECASE:IN-MEMORY:CreateCandidateUsecase", func() {
+	adapter := adapters.NewInMemoryAdapter()
+	createCandidate := usecases.NewCreateCandidateUsecase(adapter)
+
+	specs.CreateCandidateUsecase(createCandidate)
 })
